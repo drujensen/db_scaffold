97e7a547e77432b4f2c57de38cc91750073ae907
Dru Jensen <drujensen@gmail.com>
2018-02-03 10:15:35 -0800

    support --all

--- src/db_scaffold.cr
+++ src/db_scaffold.cr
@@ -4,5 +4,7 @@ if table_name = ARGV[0]?
   DbScaffold::Application.new(table_name)
 else
   puts "Error: Table Name required\n".colorize(:red)
-  puts "./bin/db_scaffold {table_name}".colorize(:cyan)
+  puts "bin/db_scaffold {table_name}".colorize(:cyan)
+  puts "or".colorize(:light_gray)
+  puts "bin/db_scaffold --all".colorize(:cyan)
 end
--- src/db_scaffold/application.cr
+++ src/db_scaffold/application.cr
@@ -1,6 +1,44 @@
+require "amber"
+require "db"
+require "mysql"
+
 class DbScaffold::Application
   def initialize(table_name)
+    if table_name == "--all"
+      table_names.each do |table_name|
+        generate table_name
+      end
+    else
+      generate table_name
+    end
+  end
+
+  private def generate(table_name : String)
     table = Table.new(table_name)
     `./bin/amber g scaffold #{table.name} #{table.fields}`
+  end
+
+  private def table_names
+    results = [] of String
+
+    if database_url = Amber.settings.database_url
+      DB.open database_url do |db|
+        db.query schema do |rs|
+          rs.each do
+            results << rs.read(String)
+          end
+        end
+      end
+    else
+      raise "database_url not found"
+    end
+
+    results
+  end
+
+  private def schema
+    return "SELECT DISTINCT table_name" \
+           " FROM information_schema.columns" \
+           " WHERE table_schema = (SELECT DATABASE());"
   end
 end
